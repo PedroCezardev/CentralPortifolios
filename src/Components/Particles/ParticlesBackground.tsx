d86e78bbb7148f68659dd7d504bbcfd4b3dc1cd6
PedroCezarDev <pcdasilvabeserra@gmail.com>
2025-02-20 22:44:57 -0300

    feat:  adicionando particles, poremm com o arquivo com 2 bugs, commit pra guardar esse projeto

--- src/Components/Particles/ParticlesBackground.tsx
+++ src/Components/Particles/ParticlesBackground.tsx
@@ -1,101 +1,38 @@
-import { useEffect } from 'react';
+import Particles from 'react-tsparticles'; // Importação correta do Particles do react-tsparticles
 
-// Tipagem para o uso do particlesJS (Se necessário)
-declare global {
-  interface Window {
-    particlesJS: any;
-  }
-}
-
-const ParticlesBackground: React.FC = () => {
-  useEffect(() => {
-    // Inicializar o particlesJS
-    if (window.particlesJS) {
-      window.particlesJS("particles", {
-        particles: {
-          number: {
-            value: 100,
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-          },
-          color: {
-            value: "#ffffff",
-          },
-          shape: {
-            type: "circle",
-            stroke: {
-              width: 0,
-              color: "#000000",
-            },
-          },
-          opacity: {
-            value: 0.8,
-            random: true,
-            animation: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0,
-              sync: false,
-            },
-          },
-          size: {
-            value: 3,
-            random: true,
-          },
-          line_linked: {
-            enable: true,
-            distance: 150,
-            color: "#ffffff",
-            opacity: 0.4,
-            width: 1,
-          },
-          move: {
-            enable: true,
-            speed: 2,
-            direction: "none",
-            random: true,
-            straight: false,
-            out_mode: "out",
-            bounce: false,
-          },
+const ParticlesBackground = () => {
+  const particlesOptions = {
+    background: {
+      color: { value: '#000000' },
+    },
+    fpsLimit: 60,
+    interactivity: {
+      events: {
+        onClick: { enable: true, mode: 'push' },
+        onHover: { enable: true, mode: 'repulse' },
+      },
+      modes: {
+        push: { distance: 10, duration: 0.4 },
+      },
+    },
+    particles: {
+      move: {
+        direction: 'none',
+        outModes: {
+          default: 'bounce',
         },
-        interactivity: {
-          detectsOn: "canvas",
-          events: {
-            onHover: {
-              enable: true,
-              mode: "push",
-            },
-            onClick: {
-              enable: true,
-              mode: "push",
-            },
-            resize: true,
-          },
-          modes: {
-            repulse: {
-              distance: 100,
-              duration: 0.4,
-            },
-            push: {
-              particles_nb: 4,
-            },
-          },
-        },
-        retina_detect: true,
-      });
-    }
-  }, []); // A dependência vazia significa que o efeito é executado apenas uma vez, na montagem.
+      },
+      number: { value: 80 },
+    },
+    detectRetina: true,
+  };
 
   return (
-    <div>
-      {/* Elemento onde o particlesJS será renderizado */}
-      <div id="particles" style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}></div>
-    
+    <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
+      <Particles id="tsparticles" options={particlesOptions} />
     </div>
   );
 };
 
 export default ParticlesBackground;
+  
--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 import style from "./Main.module.scss";
 import CardLinks from "../Components/Cards/CardLinks";
-// import Particles from "../Components/Particles/Particles";
-// import ParticlesBackground from "../Components/Particles/ParticlesBackground";
 import 'aos/dist/aos.css'; 
-import AOS from 'aos';
+import * as AOS from 'aos';
 import nextechIcon from "../assets/LogoNexTech.png";
 import perfilPedro from "../assets/PerfilPedro1x1.jpeg";
 import { BiSolidDownArrow } from "react-icons/bi";
+// import ParticlesBackground from '../Components/Particles/ParticlesBackground';
+import ParticlesComponent from '../Components/Particles/ParticlesComponent';
 
 const scrollToCards = () => {
   document.getElementById("cards")?.scrollIntoView({ behavior: "smooth" });
@@ -25,6 +25,8 @@ const Main = () => {
   return (
     <section className={style.container}>
         <nav className={style.navbar}>
+          <ParticlesComponent />
+          {/* <ParticlesBackground  /> */}
           <div className={style.content}>
             <div className={style.logo}  data-aos="fade-up" data-aos-delay="200">
               <img src={nextechIcon} alt="Logo" />
@@ -38,7 +40,7 @@ const Main = () => {
               </div>
             </div>
           </div>
-          <div onClick={scrollToCards} className={style.scroll}>
+          <div onClick={scrollToCards} className={style.scroll} data-aos="fade-up" data-aos-delay="200">
             <BiSolidDownArrow className='bx bxs-down-arrow' /> 
           </div>
         </nav>
